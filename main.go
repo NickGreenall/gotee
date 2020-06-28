e0b9bb46e266578b99404eb9d80546630c7aef8b
Nicholas Greenall <lnrg.nick@gmail.com>
2020-06-28 19:57:50 +0100

    Updated proto application to use consumer+producer

--- main.go
+++ main.go
@@ -1,44 +1,40 @@
 package main
 
-import "bufio"
-import "encoding/json"
-import "fmt"
-import "github.com/NickGreenall/gotee/internal/atomiser"
-import "github.com/NickGreenall/gotee/internal/keyEncoding"
-import "io"
-import "os"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/NickGreenall/gotee/internal/atomiser"
+	"github.com/NickGreenall/gotee/internal/consumer"
+	"github.com/NickGreenall/gotee/internal/keyEncoding"
+	"github.com/NickGreenall/gotee/internal/producer"
+	"io"
+	"os"
+)
 
 func main() {
 	rdr, wtr := io.Pipe()
 	scanner := bufio.NewScanner(os.Stdin)
 	enc := json.NewEncoder(wtr)
 	keyEnc := keyEncoding.NewJsonKeyEncoder(enc)
-	atomEnc := keyEnc.NewEncoderForKey("atom")
+	prod := producer.NewProducer(keyEnc)
+
 	dec := json.NewDecoder(rdr)
 	keyDec := keyEncoding.NewJsonKeyDecoder(dec)
-	a, err := atomiser.NewAtomiser(`(?P<dig>\d+)`, atomEnc)
+	cons := new(consumer.Consumer)
+	cons.Dec = keyDec
+	cons.Out = os.Stdout
+
+	a, err := atomiser.NewAtomiser(`(?P<dig>\d+)`, prod.AtomEnc)
 	if err != nil {
 		fmt.Printf("Unexpected error: %v", err)
 		return
 	}
-	go func() {
-		atom := make(map[string][]byte)
-		for {
-			key, err := keyDec.Pop()
-			if err != nil {
-				fmt.Printf("Unexpected error: %v", err)
-				return
-			}
-			//fmt.Println(key)
-			err = keyDec.Decode(&atom)
-			if err != nil {
-				fmt.Printf("Unexpected error: %v", err)
-				return
-			}
-			dig, _ := atom["dig"]
-			fmt.Printf("%v: %s\n", key, dig)
-		}
-	}()
+
+	go cons.Consume()
+
+	prod.SetJson()
+	//prod.SetTemplate("\033[32mdig: {{.dig}}\033[0m\n")
 	for scanner.Scan() {
 		b := scanner.Bytes()
 		//fmt.Printf("Bytes: %s\n", b)
