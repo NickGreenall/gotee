74ebb4aa283ca0d97532cca4612cb720de23bd67
Nicholas Greenall <lnrg.nick@gmail.com>
2020-08-03 14:53:41 +0100

    Dont fail on non-matches
    - Added some comments
    - Ignore no matches in main

--- main.go
+++ main.go
@@ -11,26 +11,29 @@ func main() {
 	var inStrm io.Reader
 
 	if AmForeground() {
+		// Spawn server if in foreground
 		srv, err := NewServer("unix", "./test.sock")
 		if err != nil {
 			log.Fatalln(err)
 		}
-
 		defer srv.Close()
 
 		go srv.Sniff(os.Stdout)
 
 		inStrm = os.Stdin
 	} else {
+		// Otherwise assume connected to pipe and forward on
 		inStrm = io.TeeReader(os.Stdin, os.Stdout)
 	}
 
+	// Connect to the foreground server
 	conn, err := InitConn("./test.sock")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer conn.Close()
 
+	// Setup the producer side
 	prod := InitProducer(conn)
 	prod.SetJson()
 	//prod.SetTemplate("\033[32mdig: {{.dig}}\033[0m\n")
@@ -40,10 +43,18 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	// Used to close the connection on interrupt
 	srcRdr := InitReader(inStrm, os.Interrupt)
 	defer srcRdr.Close()
 
-	Source(srcRdr, atmsr)
+	for {
+		// Source parses line by line and forwards
+		err = Source(srcRdr, atmsr)
+		_, ok := err.(*atomiser.AtomiserError)
+		if !ok {
+			break
+		}
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
